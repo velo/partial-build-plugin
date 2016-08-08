503b4c2ebc520094ff051ef6d97a6201543aea0f
Ozan Gunalp <ozangunalp@gmail.com>
2016-08-08 14:00:54 +0200

    cleanup

--- src/it/projects/downstream/setup.groovy
+++ src/it/projects/downstream/setup.groovy
@@ -1,10 +1,10 @@
+import static java.nio.charset.StandardCharsets.UTF_8
+
+import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.charset.Charset
 
 import com.lesfurets.maven.partial.mocks.UnZiper
-
-import static java.nio.charset.StandardCharsets.UTF_8
 
 def project = new File(projectDir as String)
 def zip = new File(project, "src/it/project.zip")
