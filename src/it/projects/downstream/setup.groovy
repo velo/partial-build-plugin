136522992c05165a979ee3cda4a13858dbc65a3b
ogu <Ozan.Gunalp@lesfurets.com>
2016-10-07 17:33:06 +0200

    Fix IT execution order : improved setup

--- src/it/projects/downstream/setup.groovy
+++ src/it/projects/downstream/setup.groovy
@@ -1,24 +1,8 @@
-import com.lesfurets.maven.partial.mocks.UnZiper
+import com.lesfurets.maven.partial.mocks.ITHelper
 
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
+def testProjectBasedir = basedir as File
+def pbpBaseDir = sourceDir as String
+def pbpVersion = projectVersion as String
+new ITHelper(testProjectBasedir, pbpBaseDir, pbpVersion).setupTest()
 
-import static java.nio.charset.StandardCharsets.UTF_8
-
-def project = new File(sourceDir as String)
-def zip = new File(project, "src/it/project.zip")
-new UnZiper().act(zip, basedir as File);
-
-folder = "downstream"
-Files.copy(Paths.get(sourceDir as String, "src/it/projects", folder, "pom.xml"),
-        Paths.get(basedir as String, "pom.xml"),
-        StandardCopyOption.REPLACE_EXISTING)
-
-Path path = new File(basedir as File, "pom.xml").toPath()
-String content = new String(Files.readAllBytes(path), UTF_8);
-content = content.replaceAll("@project.version@", projectVersion);
-Files.write(path, content.getBytes(UTF_8));
-
-return true;
+return true
